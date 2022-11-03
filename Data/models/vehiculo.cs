5a2fddb3e23b5ec7d7721339f8d41f98e37ed4aa
luis gabriel acosta matias <2020-0906@Uteco.edu.do>
2022-11-03 17:13:37 -0400

    0.1

---
+++ Data/models/vehiculo.cs
@@ -0,0 +1,10 @@
+namespace models;
+
+public class vehiculo
+{
+    public int vehiculoID{get; set;}
+    public string Marca{get; set;}=null;
+    public string Modelo {get; set;}=null;
+    public int año {get; set;}
+    public string Color {get; set;}=null;
+}
